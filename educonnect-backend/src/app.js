afa0209cee1fbe779301d14bc4f4ed8b3b478eae
Niraj Pandit <NirajPandit320>
2026-04-08 15:42:28 +0530

    Corrected url of netlify in app.js

--- educonnect-backend/src/app.js
+++ educonnect-backend/src/app.js
@@ -9,7 +9,10 @@ const eventRoutes = require("./routes/event.routes");
 const messageRoutes = require("./routes/message.routes");
 const notificationRoutes = require("./routes/notification.routes");
 
-app.use(cors());
+app.use(cors({
+  origin: "https://educonnect.netlify.app",
+  credentials: true
+}));
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 app.use("/uploads", express.static("uploads"));
